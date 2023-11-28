f91bf6f89126c12ef9c34b01da7484f7f6e9af39
Theodros Mulugeta <t.mulugeta@nextkidney.com>
2023-11-28 16:40:53 +0100

    Initial commit

---
+++ src/main/java/ch/heigvd/Main.java
@@ -0,0 +1,12 @@
+package ch.heigvd;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(Main.class);
+
+        logger.debug("Hello World!");
+    }
+}
