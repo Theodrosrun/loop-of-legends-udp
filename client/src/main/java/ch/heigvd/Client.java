bcbab5a4ff2a320877af49e1870a3cb23e9f6de3
Theodros Mulugeta <t.mulugeta@nextkidney.com>
2023-12-03 14:45:34 +0100

    Cleaning

--- client/src/main/java/ch/heigvd/Client.java
+++ client/src/main/java/ch/heigvd/Client.java
@@ -58,3 +58,4 @@ public class Client {
         client.test();
     }
 }
+
--- server/src/main/java/ch/heigvd/Server.java
+++ server/src/main/java/ch/heigvd/Server.java
@@ -7,24 +7,18 @@ import java.util.concurrent.ScheduledExecutorService;
 
 public class Server {
     private static final int NB_PLAYER = 4;
-    private MulticastSocket multicastSocketEmitter;
+    private MulticastSocket socket;
 
     private ScheduledExecutorService scheduler;
 
     private Server(int port, String host) {
         this.scheduler = Executors.newScheduledThreadPool(NB_PLAYER);
         try {
-            multicastSocketEmitter = new MulticastSocket(port);
-            // Convertit le nom d'hôte spécifié dans la variable host en une adresse IP
+            socket = new MulticastSocket(port);
             InetAddress multicastAddress = InetAddress.getByName(host);
-            // Représente le groupe multicast auquel le socket se joindra pour envoyer des messages.
             InetSocketAddress group = new InetSocketAddress(multicastAddress, port);
-            // Récupère une interface réseau spécifique à l'aide de son nom (fourni par interfaceName).
-            // Ceci est important pour déterminer par quelle interface réseau les données multicast seront envoyées.
             NetworkInterface networkInterface = NetworkInterfaceHelper.getFirstNetworkInterfaceAvailable();
-            // Fait rejoindre au socket multicast le groupe multicast spécifié sur l'interface réseau choisie.
-            // Cela permet au socket d'envoyer des paquets de données au groupe multicast sur l'interface réseau spécifiée.
-            multicastSocketEmitter.joinGroup(group, networkInterface);
+            socket.joinGroup(group, networkInterface);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
