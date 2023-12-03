953e17989a8376145780c20b54067e8106c02dfa
Theodros Mulugeta <t.mulugeta@nextkidney.com>
2023-12-03 14:08:39 +0100

    Cleaning class

--- client/src/main/java/ch/heigvd/Client.java
+++ client/src/main/java/ch/heigvd/Client.java
@@ -1,358 +1,55 @@
 package ch.heigvd;
 
-import com.googlecode.lanterna.input.KeyStroke;
-import java.io.*;
-import java.net.InetAddress;
-import java.net.Socket;
-import java.net.UnknownHostException;
-import java.nio.charset.StandardCharsets;
-import static java.lang.System.*;
+import java.net.*;
 
 public class Client {
-    /**
-     * The terminal
-     */
     private final Terminal terminal = new Terminal();
 
-    /**
-     * The input handler used to get the user inputs
-     */
     private final InputHandler inputHandler = new InputHandler(terminal, 50);
 
-    /**
-     * The command, response, message and data used to communicate with the server
-     */
     private String command = "", response = "", message = "", data = "";
 
-    /**
-     * The output streams used to communicate with the server
-     */
-    private BufferedWriter serverOutput;
+    private DatagramSocket socket;
 
-    /**
-     * The input stream used to communicate with the server
-     */
-    private BufferedReader serverInput;
+    private InetAddress serverAddress;
 
-    /**
-     * The socket used to communicate with the server
-     */
-    private Socket socket;
+    private int serverPort;
 
-    /**
-     * The multicast socket receiver used to communicate with the server
-     */
-    private Socket multicastSocketReceiver;
-
-    /**
-     * Constructor of the client
-     *
-     * @param address the address of the server
-     * @param port    the port of the server
-     */
-    public Client(InetAddress address, int port) {
-        initConnection(address, port);
-        tryLobby();
-        join();
-        waitReady();
-        controlSnake();
-    }
-
-    /**
-     * Create the socket
-     *
-     * @param address the address of the server
-     * @param port    the port of the server
-     */
-    private void createSocket(InetAddress address, int port) {
+    public Client(InetAddress serverAdress, int serverPort) {
         try {
-            socket = new Socket(address, port);
+            this.socket = new DatagramSocket();
+            this.serverAddress = serverAdress;
+            this.serverPort = serverPort;
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        catch (IOException e) {
-            terminal.print("Error creating the socket: " + e.getMessage() + "\n" + "Press enter to exit\n");
-            requestEnter();
-            exit(1);
-        }
-
     }
 
-    /**
-     * Initialize the connection with the server
-     *
-     * @param address the address of the server
-     * @param port    the port of the server
-     */
-    private void initConnection(InetAddress address, int port) {
+    public void sendMessage(String message) {
         try {
-            Thread thConnection = new Thread(() -> createSocket(address, port));
-            thConnection.start();
-            int timeOutTime = 5; //secondes
-            while (timeOutTime > 0 && (socket == null || !socket.isConnected())) {
-                terminal.clear();
-                terminal.print("Connecting to the server... time out in  " + timeOutTime + "second" + (timeOutTime > 1 ? "s" : ""));
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-                timeOutTime--;
-            }
-            thConnection.interrupt();
-            if (socket == null || !socket.isConnected()) {
-                terminal.clear();
-                terminal.print("Connection timeout\nPress enter to exit");
-                requestEnter();
-                exit(1);
-            }
-            serverOutput = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8));
-            serverInput = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
-
-            Thread exitTh = new Thread(new Exit(socket, serverOutput, serverInput));
-            Runtime.getRuntime().addShutdownHook(exitTh);
-
-            command = Message.setCommand(Message.INIT);
-            serverOutput.write(command);
-            serverOutput.flush();
-
-            while (!message.equals("DONE")) {
-                response = Message.getResponse(serverInput);
-                message = Message.getMessage(response);
-                data = Message.getData(response);
-                messageHandling(message, data);
-            }
-        } catch (IOException e) {
-            exit(1);
+            byte[] command = message.getBytes();
+            DatagramPacket commandPacket = new DatagramPacket(
+                    command,
+                    command.length,
+                    serverAddress,
+                    serverPort);
+            socket.send(commandPacket);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
-    /**
-     * try if lobby is open or not full
-     */
-    private void tryLobby() {
+    public void receiveMessage() {
         try {
-            serverOutput.write(Message.setCommand(Message.LOBB));
-            serverOutput.flush();
-            response = Message.getResponse(serverInput);
-            message = Message.getMessage(response);
-            data = Message.getData(response);
-            messageHandling(message, data);
-        } catch (IOException e) {
-            terminal.clear();
-            terminal.print("Client exception: " + e);
+            byte[] response = new byte[1024];
+            DatagramPacket responsePacket = new DatagramPacket(
+                    response,
+                    response.length);
+            socket.receive(responsePacket);
+            String message = new String(responsePacket.getData(), 0, responsePacket.getLength());
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-    }
-
-    /**
-     * Join the lobby
-     */
-    private void join() {
-        terminal.clear();
-        terminal.print(Intro.logo);
-        while (inputHandler.getKey() != Key.ENTER) {
-            if (inputHandler.getKey() == Key.QUIT) {
-                quit();
-            }
-            try {
-                Thread.sleep(200);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }
-
-        inputHandler.pauseHandler();
-
-        try {
-            while (true) {
-                String UserName = terminal.userInput();
-
-                if (UserName == null) {
-                    serverOutput.write(Message.setCommand(Message.QUIT));
-                    serverOutput.flush();
-                    exit(1);
-                }
-
-                if (socket.getInputStream().available() > 0) {
-                    response = Message.getResponse(serverInput);
-                    message = Message.getMessage(response);
-                    data = Message.getData(response);
-                    messageHandling(message, data);
-                }
-
-                command = Message.setCommand(Message.JOIN, UserName);
-                serverOutput.write(command);
-                serverOutput.flush();
-                response = Message.getResponse(serverInput);
-                message = Message.getMessage(response);
-                data = Message.getData(response);
-                messageHandling(message, data);
-
-                if (message.equals("DONE")) {
-                    break;
-                }
-                inputHandler.restoreHandler();
-                inputHandler.resetKey();
-                while (inputHandler.getKey() != Key.ENTER) {
-                    terminal.print(data + "\n" + "Press enter to continue\n");
-                    try {
-                        Thread.sleep(200);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-                inputHandler.pauseHandler();
-
-            }
-        } catch (IOException e) {
-            terminal.clear();
-            terminal.print("Client exception: " + e);
-        }
-
-        inputHandler.restoreHandler();
-    }
-
-    /**
-     * Wait for the game to start
-     */
-    private void waitReady() {
-        inputHandler.resetKey();
-        boolean isReady = false;
-        try {
-            while (!isReady) {
-                if (inputHandler.getKey() == Key.READY) {
-                    command = Message.setCommand(Message.RADY);
-                    serverOutput.write(command);
-                    serverOutput.flush();
-                    inputHandler.resetKey();
-                    isReady = true;
-                }
-                else if (inputHandler.getKey() == Key.HELP) {
-                    terminal.clear();
-                    terminal.print(Help.Rules + "\n" + Help.Commands);
-                    requestKey(Key.HELP);
-                }
-                if (inputHandler.getKey() == Key.QUIT) {
-                    command = Message.setCommand(Message.QUIT);
-                    quit();
-                }
-                response = Message.getResponse(serverInput);
-                message = Message.getMessage(response);
-                data = Message.getData(response);
-                messageHandling(message, data);
-                terminal.clear();
-                terminal.print(data);
-            }
-
-        } catch (IOException e) {
-            exit(1);
-        }
-    }
-
-    /**
-     * Control the snake
-     */
-    private void controlSnake() {
-        try {
-            while (inputHandler.getKey() != Key.QUIT) {
-                KeyStroke key = inputHandler.getKeyStroke();
-                if (InputHandler.isDirection(key)) {
-                    command = Message.setCommand(Message.DIRE, Key.parseKeyStroke(key).toString());
-                    serverOutput.write(command);
-                    serverOutput.flush();
-                    inputHandler.resetKey();
-                }
-                response = Message.getResponse(serverInput);
-                message = Message.getMessage(response);
-                data = Message.getData(response);
-                messageHandling(message, data);
-                terminal.clear();
-                terminal.print(data);
-            }
-            quit();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    /**
-     * Quit the game
-     */
-    private void quit() {
-        command = Message.setCommand(Message.QUIT);
-        try {
-            serverOutput.write(command);
-            serverOutput.flush();
-            response = Message.getResponse(serverInput);
-        } catch (IOException e) {
-            terminal.clear();
-            terminal.print("Client exception: " + e);
-        }
-
-        message = Message.getMessage(response);
-        data = Message.getData(response);
-        if (!message.equals("QUIT")) {
-            terminal.print("Error :" + data);
-            exit(1);
-        }
-        terminal.clear();
-        terminal.print(data + "\n" + "Press enter to exit\n");
-
-        while (inputHandler.getKey() != Key.ENTER) {
-            try {
-                Thread.sleep(200);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }
-        exit(0);
-    }
-
-    /**
-     * Handle the message
-     *
-     * @param message the message
-     * @param data    the data
-     */
-    private void messageHandling(String message, String data) {
-        switch (message) {
-            case "EROR":
-                terminal.clear();
-                terminal.print("Error :" + data + "\n" + "Press enter to exit\n");
-                requestEnter();
-                exit(0);
-                break;
-            case "QUIT":
-                terminal.clear();
-                terminal.print("Server left \n" + data + "\n" + "Press enter to exit\n");
-                requestEnter();
-                exit(0);
-            default:
-                break;
-        }
-
-    }
-
-    /**
-     * Request the user to press enter
-     */
-    private void requestEnter() {
-        requestKey(Key.ENTER);
-    }
-
-    /**
-     * Request the user to press a key
-     * @param key the key to press
-     */
-    private void requestKey(Key key){
-        inputHandler.resetKey();
-        while (inputHandler.getKey() != key) {
-            inputHandler.restoreHandler();
-            try {
-                Thread.sleep(200);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }
-        inputHandler.resetKey();
     }
 
     public static void main(String[] args) {
@@ -365,12 +62,12 @@ public class Client {
             return;
         }
 
-        InetAddress address;
-        int port;
+        InetAddress serverAdress;
+        int serverPort;
 
         // Resolve the address
         try {
-            address = InetAddress.getByName(args[0]);
+            serverAdress = InetAddress.getByName(args[0]);
         } catch (UnknownHostException ex) {
             System.err.println("Error: The address " + args[0] + " is unknown.");
             return;
@@ -378,8 +75,8 @@ public class Client {
 
         // Validate the port number
         try {
-            port = Integer.parseInt(args[1]);
-            if (port < 0 || port > 65535) {
+            serverPort = Integer.parseInt(args[1]);
+            if (serverPort < 0 || serverPort > 65535) {
                 System.err.println("Error: Port number must be between 0 and 65535.");
                 return;
             }
@@ -390,7 +87,7 @@ public class Client {
 
         // Create the client
         try {
-            Client client = new Client(address, port);
+            Client client = new Client(serverAdress, serverPort);
         } catch (Exception ex) {
             System.err.println("Error creating the client: " + ex.getMessage());
         }
--- server/src/main/java/ch/heigvd/Server.java
+++ server/src/main/java/ch/heigvd/Server.java
@@ -8,82 +8,30 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
-/**
- * The class that represent the server
- */
 public class Server {
-    /**
-     * The number of players in the game
-     */
     private static final int NB_PLAYER = 4;
 
-    /**
-     * The frequency that refresh the game in milliseconds
-     */
-    private final int GAME_FREQUENCY = 200;
-
-    /**
-     * The logger
-     */
-    private final static Logger LOG = Logger.getLogger(Server.class.getName());
-
-    /**
-     * The port used by the server
-     */
     private final int port;
 
-    /**
-     * The Subnet range/multicast address to use.
-     */
     private final String host;
 
-    /**
-     * The multicast socket emitter used to communicate with clients
-     */
     private MulticastSocket multicastSocketEmitter;
 
-    /**
-     * The scheduled Executor Service
-     */
     private ScheduledExecutorService scheduler;
 
-    /**
-     * The boolean that indicates if the server is listening for new clients
-     */
     private boolean listenNewClient = true;
 
-    /**
-     * The lobby
-     */
     private Lobby lobby = new Lobby(NB_PLAYER);
 
-    /**
-     * The board
-     */
     private Board board;
 
-    /**
-     * Pool of thread
-     */
     ArrayList<Thread> pool = new ArrayList<>();
 
-    //    /**
-    //     * The directions initialized in the order UP, RIGHT, DOWN, LEFT for the first 4 players
-    //     */
-    //    private Direction[] directions = {Direction.UP, Direction.RIGHT, Direction.DOWN, Direction.LEFT};
-
-    /**
-     * The constructor
-     *
-     * @param port The port used by the server
-     */
     private Server(int port, String host) {
         this.port = port;
         this.host = host;
-        this.scheduler = Executors.newScheduledThreadPool(4);
+        this.scheduler = Executors.newScheduledThreadPool(NB_PLAYER);
         try {
             multicastSocketEmitter = new MulticastSocket(port);
             // Convertit le nom d'hôte spécifié dans la variable host en une adresse IP
@@ -98,185 +46,6 @@ public class Server {
             multicastSocketEmitter.joinGroup(group, networkInterface);
         } catch (IOException e) {
             throw new RuntimeException(e);
-        }
-    }
-
-    /**
-     * Ask if the username is already in use
-     *
-     * @return true if the username is already in use
-     */
-    public boolean playerNameAlreadyInUse(String userName) {
-        return lobby.playerNameAlreadyInUse(userName);
-    }
-
-    /**
-     * Join the lobby
-     *
-     * @param player The player that wants to join the lobby
-     */
-    public void joinLobby(Player player) {
-        lobby.join(player);
-        board.deployLobby(lobby);
-    }
-
-    /**
-     * Ask if lobby is full
-     *
-     * @return true if lobby is full
-     */
-    public boolean isLobbyFull() {
-        return lobby.lobbyIsFull();
-    }
-
-    /**
-     * remove player from lobby
-     */
-    public void removePlayerFromLobby(Player player) {
-        lobby.removePlayer(player);
-    }
-
-    /**
-     * Get the infos of the lobby
-     *
-     * @return The infos of the lobby
-     */
-    public String getLobbyInfos() {
-        return lobby.getInfos();
-    }
-
-    /**
-     * Set the player ready
-     *
-     * @param player The player that is ready
-     */
-    public void setPlayerReady(Player player) {
-        lobby.setReady(player);
-        board.deployLobby(lobby);
-    }
-
-    /**
-     * Get the frequency that refresh the game in milliseconds
-     *
-     * @return The frequency that refresh the game in milliseconds
-     */
-    public int getGameFrequency() {
-        return GAME_FREQUENCY;
-    }
-
-    /**
-     * Get the board
-     *
-     * @return The board
-     */
-    public Board getBoard() {
-        return board;
-    }
-
-    /**
-     * Set the direction of the player
-     *
-     * @param key    The key pressed by the player
-     * @param player The player that pressed the key
-     */
-    public void setDirection(Key key, Player player) {
-        if (!lobby.everyPlayerReady()) return;
-        Direction direction = Direction.parseKey(key);
-        if (direction != null) {
-            lobby.setDirection(player, direction);
-        }
-    }
-
-    /**
-     * Listen for new clients
-     */
-    private void listenNewClient() {
-        try (ServerSocket serverSocket = new ServerSocket(port)) {
-            while (listenNewClient) {
-                LOG.log(Level.INFO, "Waiting for a new client on port {0}", port + " with PID: " + ProcessHandle.current().pid());
-                Socket clientSocket = serverSocket.accept();
-
-                if (lobby.isOpen() && !lobby.lobbyIsFull()) {
-                    LOG.info("A new client has arrived. Starting a new thread and delegating work to a new servant...");
-                    Thread th = new Thread(new ServerWorker(clientSocket, this));
-                    pool.add(th);
-                    th.start();
-                    continue;
-                }
-
-                BufferedWriter serverOutput = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream(), StandardCharsets.UTF_8));
-
-                if (lobby.lobbyIsFull()) {
-                    LOG.log(Level.INFO, "The lobby is full. Rejecting the new client...");
-                    serverOutput.write(Message.setCommand(Message.EROR, "The lobby is full"));
-                    serverOutput.flush();
-                    clientSocket.close();
-                    continue;
-                } else {
-                    LOG.log(Level.INFO, "The lobby is closed. Rejecting the new client...");
-                    serverOutput.write(Message.setCommand(Message.EROR, "The lobby is closed"));
-                    serverOutput.flush();
-                    clientSocket.close();
-                    continue;
-                }
-            }
-        } catch (IOException ex) {
-            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
-    /**
-     * Start the server
-     */
-    private void start() {
-
-        while (true) {
-            listenNewClient = true;
-            Thread thListener = new Thread(this::listenNewClient);
-            thListener.start();
-
-            board = new Board(30, 15, 15, 200);
-
-            // Loop for lobby
-            lobby.open();
-            while (!lobby.everyPlayerReady()) {
-                board.deployLobby(lobby);
-                try {
-                    Thread.sleep(GAME_FREQUENCY);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-            lobby.initSnakes(board);
-            lobby.close();
-            listenNewClient = false;
-            thListener.interrupt();
-
-            // Loop for game
-            ArrayList<Position> generatedFood = new ArrayList<>();
-            while (lobby.getNbPlayer() > 0) {
-                board.initBoard();
-                generatedFood.clear();
-                lobby.snakeStep();
-                for (Player player : lobby.getPlayers()) {
-                    if (!player.isAlive()) continue;
-                    for (Player opponent : lobby.getPlayers()) {
-                        if (player != opponent) {
-                            {
-                                generatedFood.addAll(opponent.getSnake().attack(player.getSnake()));
-                            }
-                        }
-                    }
-                }
-                board.setFood(generatedFood);
-                board.deploySnakes(lobby.getSnakes());
-                board.deployFood();
-                try {
-                    Thread.sleep(GAME_FREQUENCY);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            }
         }
     }
 
